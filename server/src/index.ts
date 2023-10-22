16d9e4c9db70b6d4c05c6c26b12dbf46fe7b6b20
Abdel <abdelisloading@gmail.com>
2023-10-22 17:16:50 +0100

    Bootstrap backend and connect it to MongoDB

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,40 @@
 import express from "express";
+import dotenv from "dotenv";
+import http from "http";
+import { resolve } from "path";
+import mongoose from "mongoose";
 
-const app = express();
-const port = 8080;
+async function bootstrap(): Promise<void> {
+  dotenv.config({
+    path: ".env.production",
+  });
+  const port: number = Number(process.env.PORT) || 8080;
+  const __dirname: string = resolve();
+  let error: string = "";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+  const app: express.Express = express();
+  const server: http.Server<
+    typeof http.IncomingMessage,
+    typeof http.ServerResponse
+  > = http.createServer(app);
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((_: Error) => {
+      console.error("Error connecting to MongoDB");
+      error += " Error connecting to MongoDB";
+    });
+
+  app.get("/", (_: express.Request, res: express.Response) => {
+    res.status(200).send(`Welcome to easypicks API!${error}`);
+  });
+
+  server.listen(port, () => {
+    return console.log(`Server is listening on ${port}`);
+  });
+}
+
+bootstrap().catch((err: Error) => {
+  console.error(err);
 });
